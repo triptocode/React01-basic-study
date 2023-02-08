46f3df2a56a24a921894709989c90bc2db32e0d0
triptocode <3580rio@gmail.com>
2023-02-08 21:34:11 +0900

    react basic

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          first react project !!
+          first react project !
         </p>
         <a
           className="App-link"
