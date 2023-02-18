3e9e28e5f88dce682e64da93bd10463be9974e6b
triptocode <3580rio@gmail.com>
2023-02-19 04:06:37 +0900

    jsx props useState

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  //const text = 'JSX & React project !!'
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          first react project
-          {/* {text} */}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import React from "react";
+// import Counter from "./Counter";
 
-export default App;
+// function App(){
+//   return (
+//     <Counter/>
+//   )
+// }
+
+// export default App;
 
 
 
+// - 01
+// import logo from './logo.svg';
+// import './App.css';
 
+// function App() {
+//   //const text = 'JSX & React project !!'
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           first react project
+//           {/* {text} */}
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// - 02
 // import logo from './logo.svg';
 // import './App.css';
 // import React from 'react';
@@ -42,3 +57,15 @@ export default App;
 // }
 
 // export default App;
+
+
+// - 3
+import React from "react";
+import "./App.css";
+import MyComponent from "./MyComponent";
+
+function App() {
+  return <MyComponent />;
+}
+
+export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyComponent = () => {
+  return <div> 컴포넌트의 export, import 테스트 페이지</div>;
+};
+
+export default MyComponent;
