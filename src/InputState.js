51161b20dbf6b7c631fe98cb6171a9d849c5b591
triptocode <3580rio@gmail.com>
2023-02-24 20:02:32 +0900

    jsx, props, useState

--- src/InputState.js
+++ src/InputState.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 
 function InputState() {
   const [text, setText] = useState('');
-
   const onChange = (e) => {
     setText(e.target.value);
+    //console.log(e); // e는 SyntheticzEvent ! 
+    //console.log(e.target); // 
   };
-
   const onReset = () => {
     setText('');
   };
@@ -14,12 +14,13 @@ function InputState() {
   return (
     <div>
       <input onChange={onChange} value={text}  />
-      <button onClick={onReset}>초기화</button>
+      <button onClick={onReset}>초기화</button> 
       <div>
         <b>값: {text}</b>
       </div>
     </div>
   );
+
 }
 
 export default InputState;
